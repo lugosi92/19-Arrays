915fd2fb746edbd47784c5b5f048e608fd28cac9
khaled___beno <khaled___beno@localhost>
2023-12-26 19:40:12 +0100

    Arrays

---
+++ 19_Arreglos/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] arregloEnteros = new int[10];
+		
+		arregloEnteros[1] = 20;
+		arregloEnteros[2] = 200;
+		System.out.println(arregloEnteros[2] - arregloEnteros[1]);
+	}
+
+}
